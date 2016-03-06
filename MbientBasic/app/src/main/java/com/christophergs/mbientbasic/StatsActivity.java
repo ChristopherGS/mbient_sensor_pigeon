a1aac2d4680e211b698f3c320b9a650805dc667a
ChristopherGS <christopher.samiullah@gmail.com>
2016-03-06 21:54:05 +0800

    GET request to server ML algorithms working

--- MbientBasic/app/src/main/java/com/christophergs/mbientbasic/StatsActivity.java
+++ MbientBasic/app/src/main/java/com/christophergs/mbientbasic/StatsActivity.java
@@ -1,20 +1,27 @@
 package com.christophergs.mbientbasic;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothDevice;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.BarChart;
@@ -34,7 +41,19 @@ import com.github.mikephil.charting.utils.PercentFormatter;
 import com.mbientlab.metawear.MetaWearBleService;
 
 
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class StatsActivity extends AppCompatActivity {
@@ -63,6 +82,7 @@ public class StatsActivity extends AppCompatActivity {
         Log.i(TAG, String.format("Experiment ID: %s", EXPERIMENT_ID));
         setContentView(mChart);
         mChart.setDescription("Grappling Position Analysis");
+        getAnalysis();
         //addData(); //causes null pointer error
 
         ArrayList<Entry> yVals1 = new ArrayList<Entry>();
@@ -115,6 +135,92 @@ public class StatsActivity extends AppCompatActivity {
     private void addData(){
 
 
+    }
+
+    public void getAnalysis() {
+        toastIt("Getting analysis from server");
+        try {
+            URL url = new URL("http://christophergs.pythonanywhere.com/api/analyze/"+EXPERIMENT_ID);
+            new DownloadFilesTask().execute(url);
+        } catch (Exception e) {
+            Log.e(TAG, "file send error", e);
+            toastIt("File sending error!");
+        }
+
+    }
+
+    public class DownloadFilesTask extends AsyncTask<URL, Void, String> {
+        private ProgressDialog pDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(StatsActivity.this);
+            pDialog.setMessage("Analyzing Data");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(URL... my_url) {
+
+            HttpURLConnection urlConnection = null;
+            String boundary = "*****";
+            DataInputStream inputStream = null;
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+
+            String responseText = null;
+            String serverResponseMessage = null;
+            try {
+                URL url = my_url[0];
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Content-Language", "en-US");
+                urlConnection.setUseCaches(false);
+                urlConnection.setDoInput(true);
+                urlConnection.setDoOutput(false);
+
+                int status = urlConnection.getResponseCode();
+                InputStream response = urlConnection.getInputStream();
+
+                // Responses from the server (code and message)
+                responseText = urlConnection.getResponseMessage();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response));
+                StringBuilder sb = new StringBuilder();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                serverResponseMessage = sb.toString();
+                Log.i(TAG, String.format("full server response: %s", serverResponseMessage));
+                response.close();
+
+            } catch (Exception e) {
+                Log.e(TAG, "Analysis error", e);
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+            }
+
+            return responseText;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            pDialog.dismiss();
+            Log.i(TAG, String.valueOf(result));
+            toastIt(result);
+        }
+
+    }
+
+    private void toastIt(String msg) {
+        Toast.makeText(StatsActivity.this, msg, Toast.LENGTH_SHORT).show();
     }
 
 
